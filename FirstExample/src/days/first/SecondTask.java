5dece658676c23d851d0267ca18fa3f223253e0d
andbal <andriusbaltrunas@yahoo.com>
2020-04-16 19:38:25 +0300

    add few new lines

--- FirstExample/src/days/first/SecondTask.java
+++ FirstExample/src/days/first/SecondTask.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class SecondTask {
 	public static void main(String[] args) {
 
-
 		System.out.println("Iveskite norima polindroma, o mes patikrinsim");
 		Scanner scanner = new Scanner(System.in);
 
